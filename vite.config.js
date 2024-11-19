ee51e3cc6202221363d6016ca9ad32d9c43f891f
samueltomen <tomennana.samuel@outlook.fr>
2024-11-19 12:57:21 +0100

    init commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '192.168.34.101',
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@/components': '/src/components',
+      '@/utils': '/src/utils',
+      '@/hooks': '/src/hooks',
+    },
+  },
+})
