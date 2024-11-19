c91ca83574d813e31d4582c7a0108444018f6a32
samueltomen <tomennana.samuel@outlook.fr>
2024-11-19 16:57:07 +0100

    clean nav, sidemenu and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './route/router'
-import 'react-quill/dist/quill.snow.css';
-import 'react-circular-progressbar/dist/styles.css';
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./route/router";
+import "react-quill/dist/quill.snow.css";
+import "react-circular-progressbar/dist/styles.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-datetime/css/react-datetime.css";
-import NavigationProvider from './contentApi/navigationProvider';
-import SideBarToggleProvider from './contentApi/sideBarToggleProvider';
-import ThemeCustomizer from './components/shared/ThemeCustomizer';
+import NavigationProvider from "./contentApi/navigationProvider";
+import SideBarToggleProvider from "./contentApi/sideBarToggleProvider"; // import ThemeCustomizer from './components/shared/ThemeCustomizer';
+// import ThemeCustomizer from './components/shared/ThemeCustomizer';
 
 const App = () => {
   return (
@@ -18,9 +18,9 @@ const App = () => {
           <RouterProvider router={router} />
         </SideBarToggleProvider>
       </NavigationProvider>
-      <ThemeCustomizer />
+      {/*<ThemeCustomizer /> Permet de personaliser rapidment le thème */}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
